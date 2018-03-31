dbd87e4fdaac2a6ff7f0c4465220c59d474435a6
TR <tianruei.kuan@gmail.com>
2018-03-31 13:26:28 +0800

    update Story

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View, Image} from 'react-native';
+import {StyleSheet, Text, View, Image, Dimensions} from 'react-native';
 import theme from '../theme.js';
 
 export default class Story extends React.Component {
@@ -7,10 +7,12 @@ export default class Story extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Image style={styles.image}
-            source={require('../images/rocket.png')}
-        />
-        <Text>Hi</Text>
+        <View style={styles.imageBlock}>
+          <Image style={{height:'100%', width: '100%'}} source={require('../images/Lenna.jpg')} />
+        </View>
+        <View style={styles.article}>
+          <Text style={styles.articalContent}>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</Text>
+        </View>
       </View>
     );
   }
@@ -21,10 +23,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: theme.themeColorLight,
+    backgroundColor: theme.themeColorLight
   },
-  image: {
+  imageBlock: {
     flex: 1,
-    width: '100%'
+    width: '100%',
+    height: Dimensions.get('window').width
+  },
+  article: {
+    width: '100%',
+    padding: 40
+  },
+  articalContent: {
+    color: theme.themeColorDark
   }
 });
